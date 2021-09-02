894a8921ece0fb4fc06c14a8f8d30484cefd0645
calebkimsd <89210382+calebkimsd@users.noreply.github.com>
2021-09-02 09:04:15 -0700

    final code

--- src/projects.java
+++ src/projects.java
@@ -1,1 +1,16 @@
+package src;
 
+public class projects {
+    public static void main(String[] args) {
+
+        float grade1 = 78;
+        float grade2 = 84;
+        float grade3 = 89;
+        float grade4 = 97;
+        float grade5 = 70;
+        float avg12 = (grade1 + grade2) / 2, avg34 = (grade3 + grade4) / 2;
+        float finalAvg = ((avg12 * 2) + (avg34 * 2) + grade5) / 5;
+
+        System.out.println(finalAvg);
+    }
+}
